ec6be18bc3d6e862346113a6d311c8f393431516
Osauyi <victorimafidon17@gmail.com>
2022-11-03 19:45:03 +0100

    Removed notfiNumber

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const MarkAll = document.querySelector(".MarkR")
+const NotiNumb = document.querySelector(".headNumb")
 const UnRead = document.querySelectorAll(".top3")
 const RemoveDot = document.querySelectorAll(".redDot")
 
@@ -8,11 +9,11 @@ MarkAll.addEventListener("click", ReadAll)
 function ReadAll() {
   for (let i = 0; i < UnRead.length; i++){
     UnRead[i].style.backgroundColor = "hsl(0, 0%, 100%)"
-  }
+  };
   for (let i = 0; i < RemoveDot.length; i++){
     RemoveDot[i].style.display = "none"
-  }
-
+  };
+  NotiNumb.style.display = "none";
   
  
 }
